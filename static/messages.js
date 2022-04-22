ba22a293375a297559d8289f596deaa91fbfef81
Dudoserovich <mr.egoryk@mail.ru>
2022-04-22 16:28:35 +1000

    Исправлена возможность написания тегов

--- static/messages.js
+++ static/messages.js
@@ -1,12 +1,20 @@
 function sendMessage(message = document.querySelector('#inputMessage').value) {
-    if (message.trim().length !== 0) {
-        fetch(window.location.href + "?message=" +
-            message.trim())
-            .then(() => {
-                window.location.reload()
-            });
-    } else {
+    // Я вам запрещаю писать сюда html теги и php код!!!
+    const regexHTML = /<\/?\??\w+[^>]*(>|\?>|$)/gi
+
+    if (message.match(regexHTML)) {
+        alert("Если хотите написать тег, '<' или '>', пишите с пробелами")
         document.querySelector('#inputMessage').value = ""
+    } else {
+        if (message.trim().length !== 0) {
+            fetch(window.location.href + "?message=" +
+                message.trim())
+                .then(() => {
+                    window.location.reload()
+                });
+        } else {
+            document.querySelector('#inputMessage').value = ""
+        }
     }
 }
 
